8b47f1d4e61e3155d26e4099afe22e9308ba6ea3
kevinmbongo <mbongo.kevin.pro@gmail.com>
2022-04-07 01:59:35 +0200

    feat(Card):implement Card

--- src/Pages/Liste/Liste.js
+++ src/Pages/Liste/Liste.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { animated, useTrail } from "react-spring";
+import { v4 as uuidv4 } from "uuid";
+import Card from "../../Components/Card/Card";
 import "./Liste.css";
 
 export default function Liste() {
-  return <div>Liste</div>;
+  const trail = useTrail(9, {
+    from: { opacity: 0, x: 20 },
+    to: { opacity: 1, x: 0 },
+  });
+  console.log(trail);
+
+  return (
+    <div className="list-container">
+      {trail.map((cardStyle, index) => {
+        return (
+          <animated.div key={uuidv4()} style={cardStyle}>
+            <Card />
+          </animated.div>
+        );
+      })}
+    </div>
+  );
 }
