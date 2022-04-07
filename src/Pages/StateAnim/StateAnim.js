6df90f2d1dd72e0ff7fa56ca48ddbbeae31a116a
kevinmbongo <mbongo.kevin.pro@gmail.com>
2022-04-07 03:32:20 +0200

    feat(StateAnim): implemnt StateAnim

--- src/Pages/StateAnim/StateAnim.js
+++ src/Pages/StateAnim/StateAnim.js
@@ -1,6 +1,73 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { animated, useTransition } from "react-spring";
+import { v4 as uuidv4 } from "uuid";
 import "./StateAnim.css";
 
 export default function StateAnim() {
-  return <div>StateAnim</div>;
+  const [firstDisplay, setFirstDisplay] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFirstDisplay(false);
+    }, 1000);
+  }, []);
+
+  const [inputData, setInputData] = useState([
+    {
+      id: uuidv4(),
+      txt: "Chopin",
+    },
+
+    {
+      id: uuidv4(),
+      txt: "Mozart",
+    },
+
+    {
+      id: uuidv4(),
+      txt: "Bach",
+    },
+  ]);
+
+  const inputRef = useRef();
+
+  const handleData = (e) => {
+    e.preventDefault();
+
+    const newObj = {
+      id: uuidv4(),
+      txt: inputRef.current.value,
+    };
+
+    setInputData([...inputData, newObj]);
+
+    inputRef.current.value = " ";
+  };
+
+  const listTransitions = useTransition(inputData, {
+    from: { opacity: 0, transform: "translateY(10px)" },
+    enter: { opacity: 1, transform: "translateY(0px)" },
+    keys: inputData.map((item) => item.id),
+  });
+
+  return (
+    <form onSubmit={handleData}>
+      <label htmlFor="piano">Renseigner vos pianiste favorie.</label>
+      <input ref={inputRef} type="text" id="piano" />
+
+      {firstDisplay ? (
+        <ul>
+          {inputData.map((item) => (
+            <li key={item.id}>{item.txt}</li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {listTransitions((styles, item) => {
+            return <animated.li style={styles}>{item.txt}</animated.li>;
+          })}
+        </ul>
+      )}
+    </form>
+  );
 }
